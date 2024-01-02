197ed08e952f47ebdc62647ba4891a33196b42f2
RuthDanielaAguirre <argdiee@icloud.com>
2024-01-02 13:43:51 +0100

    cambio skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,21 @@ function linkAction(){
 
 navlink.forEach(n => n.addEventListener('click', linkAction))
 
+
+/*************skills***************/
+const skillsContent = document.getElementsByClassName('skills_content'),
+    skillsHeader = document.querySelectorAll('.skills_header')
+
+    function toggleSkills (){
+        let itemClass = this.parentNode.getElementsByClassName
+        for(i=0; i<skillsContent.length; i++){
+            skillsContent[i].className = 'skills_content skills_close'
+        }
+        if(itemClass === 'skills_content skills_close'){
+            this.parentNode.className = 'skills_content skills_open'
+        }
+    }
+
 // button.addEventListener('click', ()=>{
 //     nav.classList.toggle('active')
 // });
